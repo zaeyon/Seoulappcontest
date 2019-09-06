ff1b416b10111e13d54ac224fcb120a67d5ec113
hyesung <comet_0419@naver.com>
2019-09-07 03:35:27 +0900

    tvEditProfile OnClickListener

--- app/src/main/java/com/example/seoulapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/seoulapp/ui/notifications/NotificationsFragment.java
@@ -1,18 +1,15 @@
 package com.example.seoulapp.ui.notifications;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.seoulapp.R;
 
--- app/src/main/java/com/example/seoulapp/ui/notifications/SettingsActivity.java
+++ app/src/main/java/com/example/seoulapp/ui/notifications/SettingsActivity.java
@@ -1,15 +1,30 @@
 package com.example.seoulapp.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.seoulapp.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView tvEditProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        tvEditProfile = (TextView)findViewById(R.id.tvEditProfile);
+        tvEditProfile.setOnClickListener(new goEditProfile());
+    }
+
+    class goEditProfile implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intentEditProfile = new Intent(SettingsActivity.this, EditProfileActivity.class);
+            startActivity(intentEditProfile);
+        }
     }
 }
